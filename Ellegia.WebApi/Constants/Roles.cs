02c265816d253adf3e9d675b8214c672055f430d
Evgeny <evgenykuznetsov95@gmail.com>
2017-11-30 21:08:46 +0600

    add Roles

---
+++ Ellegia.WebApi/Constants/Roles.cs
@@ -0,0 +1,12 @@
+﻿namespace Ellegia.WebApi.Constants
+{
+    public class Roles
+    {
+        private Roles() { }
+
+        public const string Secretary = "Secretary";
+        public const string Technologist = "Technologist";
+        public const string WarehouseManager = "WarehouseManager";
+        public const string Director = "Director";
+    }
+}
