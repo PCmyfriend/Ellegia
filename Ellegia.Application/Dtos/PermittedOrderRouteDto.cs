e496edc4e61833c6c2364ba1e3700bdce665643e
Evgeny <evgenykuznetsov95@gmail.com>
2018-06-24 18:21:18 +0600

    Add permittedOrderRoute

---
+++ Ellegia.Application/Dtos/PermittedOrderRouteDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ellegia.Application.Dtos
+{
+    public class PermittedOrderRouteDto
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+    }
+}
