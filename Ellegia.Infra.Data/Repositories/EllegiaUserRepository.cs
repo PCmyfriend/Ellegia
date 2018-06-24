fd3da896750de2bcb12b9ae66f716f6d88d69853
Evgeny <evgenykuznetsov95@gmail.com>
2018-06-24 15:27:01 +0600

    Fix add ellegiaUserRepository

---
+++ Ellegia.Infra.Data/Repositories/EllegiaUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Ellegia.Domain.Models;
+using Ellegia.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ellegia.Infra.Data.Repositories
+{
+    public class EllegiaUserRepository : Repository<EllegiaUser>
+    {
+        public EllegiaUserRepository(EllegiaContext context) : base(context)
+        {
+
+        }
+
+        public override IQueryable<EllegiaUser> GetAll()
+        {
+            return base.GetAll()
+                .Include(u => u.UserRoles)
+                .ThenInclude(ur => ur.Role);
+        }
+    }
+}
